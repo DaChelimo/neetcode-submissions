7c0d72c116362d0475f74e406eede379d33f2a21
Andrew Chelimo <62815445+DaChelimo@users.noreply.github.com>
2026-08-06 18:19:49 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+class Solution:
+    
+    # Create a dict of key (alpha): value (num of occurrences)
+    # Compare both dict; return True if similar
+    # Edge Case: Both Empty; Length differs, short circuit
+
+    # Time: O(n). Space: O(n)
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+
+        for char in s:
+            s_dict[char] += 1
+
+        for char in t:
+            t_dict[char] += 1
+
+        return s_dict == t_dict
+
+        
