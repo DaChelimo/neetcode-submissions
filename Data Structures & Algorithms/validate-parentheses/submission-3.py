a026b31582de96b9bb1b6b1a86b6d3ec0ac28a23
Andrew Chelimo <62815445+DaChelimo@users.noreply.github.com>
2026-08-14 12:09:14 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+
+    # INPUT:  str s
+    # OUTPUT: bool -> true, 1. (), 2. Correct order. 3. Closing brack <-> Opening brack
+
+    # PLAN:
+    # 1. Build a stack: (, {, [
+    # 2. Append any new open parans to the stack
+    # 3. If we find closing, we try popping (if it matches top of the stack)
+    # 4. If closing and does not pop stack, return False .... {[)]}
+
+    # Edge cases: Empty string -> true. 1 char -> False. Same applies to odd
+    # Edge cases: "]})))" or "[[("
+    def isValid(self, s: str) -> bool:
+        stack = []
+        opens = {'{', '(', '['}
+        closes = {'}', ']', ')'}
+        book = {
+            '}': '{',
+            ']' : '[',
+            ')' : '('
+        }
+
+        if len(s) % 2 == 1:
+            return False
+
+        for c in s:
+            if c in opens:
+                stack.append(c)
+            
+            if c in closes:
+                latest_open = stack.pop() if stack else None
+                
+                if book[c] != latest_open:
+                    return False
+        
+        return False if stack else True
+        
