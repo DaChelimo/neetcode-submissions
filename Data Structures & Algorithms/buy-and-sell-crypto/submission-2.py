3b95bbe94852c6d7b06477e1e7c1743895230e28
Andrew Chelimo <62815445+DaChelimo@users.noreply.github.com>
2026-08-19 19:19:54 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,59 @@
+class Solution:
+    # INPUT: list of prices
+    # OUTPUT: Int  -> max profit [max(0, profit)]
+
+    # Constraints: Decreasing list -> 10, 8, 2 -> Profit: 0
+                #  Empty list -> 0
+
+    # # PLAN
+    # # 1. Create two pointers (l = 0 and r = 1), and max_p
+    # # 2. Keep l at the lowest we find
+    # # 3. Loop through array (r), we check if (prices[r] < prices[l]) => l = r, r = r + 1
+    # # 4. Else, get profit = p[r] - p[l], compare max_p, update accordingly
+    # # return max_p
+
+    # # Time: O(n). Space: O(1)
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     max_profit = 0
+
+    #     if len(prices) < 2:
+    #         return 0
+
+    #     l = 0
+    #     r = 1
+
+    #     while r < len(prices):
+    #         if prices[r] < prices[l]:
+    #             l = r
+    #         else:
+    #             curr_profit = prices[r] - prices[l]
+    #             max_profit = max(curr_profit, max_profit)
+            
+    #         r += 1    
+        
+    #     return max_profit
+
+    # PLAN
+    # 1. Keep track of the minimum value (minBuy) and maxProfit
+    # 2. Loop through entire list: if value < minBuy, minBuy = value. 
+    #    In every iteration, profit = value - minBuy. If profit > maxProfit, update maxProfit
+    # 3. Return maxProfit
+
+    # Time: O(n). Space: O(1)
+    def maxProfit(self, prices: List[int]) -> int:
+        if (len(prices) < 2):
+            return 0
+
+        min_buy = prices[0]
+        max_profit = 0
+
+        for price in prices[1:]:
+            if price < min_buy:
+                min_buy = price
+            else:
+                profit = price - min_buy
+                max_profit = max(profit, max_profit)
+        
+        return max_profit
+
+        
